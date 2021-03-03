9ebfc4e184da66da13b489e1b6076f3b42f3e819
Ahmed-nidhal <78449998+Ahmed-nidhal@users.noreply.github.com>
2021-03-03 00:17:24 -0500

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,19 @@
+import os
+import csv
+file_to_load = os.path.join("Resources","election_results.csv")
+with open(file_to_load) as election_data:
+    file_reader=csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+    # for row in file_reader:
+    #   print(row)
+
+file_to_save = os.path.join("Resources","election_analysis.txt")
+with open(file_to_save,"w") as txt_file:
+    txt_file.write("Counties in the Election\n----------------\nArapahoe\nDenver\nJefferson")
+
+    
+
+    
+
+
